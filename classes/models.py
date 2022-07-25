32ea654e352b77a201660c5425808fa0d5891828
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-07-25 19:34:00 +0300

    my Task

--- classes/models.py
+++ classes/models.py
@@ -4,10 +4,9 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length=30)
 
-
 class Lecture(models.Model):
     name = models.CharField(max_length=30)
-
+    course= models.ForeignKey(Course,on_delete=models.CASCADE, related_name="courses")
     def __str__(self):
         return self.name
 
@@ -15,6 +14,7 @@ class Lecture(models.Model):
 class Slide(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
+    lectures = models.OneToOneField(Lecture,on_delete=models.CASCADE, primary_key=True,)
 
     def __str__(self):
         return self.name
@@ -23,13 +23,16 @@ class Slide(models.Model):
 class Assignment(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
-
+    lec = models.OneToOneField(Lecture,on_delete=models.CASCADE, primary_key=True,)
+    
     def __str__(self):
         return self.name
 
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=30)
+    courses =  models.ManyToManyField(Course,related_name= "tags")
 
     def __str__(self):
         return self.name
