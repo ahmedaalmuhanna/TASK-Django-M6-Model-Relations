28056a1699c38c5e9a34f13d223b1aef67d603cb
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-07-26 13:49:14 +0300

    Bonus

--- classes/models.py
+++ classes/models.py
@@ -1,38 +1,71 @@
-from django.db import models
+# from django.db import models
 
 
-class Course(models.Model):
-    name = models.CharField(max_length=30)
-
-class Lecture(models.Model):
-    name = models.CharField(max_length=30)
-    course= models.ForeignKey(Course,on_delete=models.CASCADE, related_name="courses")
-    def __str__(self):
-        return self.name
+# class Course(models.Model):
+#     name = models.CharField(max_length=30)
 
 
-class Slide(models.Model):
-    name = models.CharField(max_length=30)
-    link = models.URLField()
-    lectures = models.OneToOneField(Lecture,on_delete=models.CASCADE, primary_key=True,)
-
-    def __str__(self):
-        return self.name
-
-
-class Assignment(models.Model):
-    name = models.CharField(max_length=30)
-    link = models.URLField()
-    lec = models.OneToOneField(Lecture,on_delete=models.CASCADE, primary_key=True,)
-    
-    def __str__(self):
-        return self.name
+# class Lecture(models.Model):
+#     name = models.CharField(max_length=30)
+#     course= models.ForeignKey(Course,on_delete=models.CASCADE, related_name="courses")
+#     def __str__(self):
+#         return self.name
 
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=30)
-    courses =  models.ManyToManyField(Course,related_name= "tags")
+# class Slide(models.Model):
+#     name = models.CharField(max_length=30)
+#     link = models.URLField()
+#     lectures = models.OneToOneField(Lecture,on_delete=models.CASCADE, primary_key=True,)
+#     def __str__(self):
+#         return self.name
 
-    def __str__(self):
-        return self.name
+
+# class Assignment(models.Model):
+#     name = models.CharField(max_length=30)
+#     link = models.URLField()
+#     lec = models.OneToOneField(Lecture,on_delete=models.CASCADE, primary_key=True,)
+#     def __str__(self):
+#         return self.name
+
+
+
+# class Tag(models.Model):
+#     name = models.CharField(max_length=30)
+#     courses =  models.ManyToManyField(Course,related_name= "tags")
+#     def __str__(self):
+#         return self.name
+
+
+class Assignment():
+    def __init__(self,name,link):
+        super().__init__(name,link)
+
+
+class Slide(Assignment):
+    def __init__(self,name,link,Lec_name):
+        super().__init__(Lec_name)
+        self.name = name
+        self.link = link
+
+
+
+
+class Lecture(Slide):
+     def __init__(self,Lec_name, slide_name, slide_link):
+         super().__init__(slide_name, slide_link)
+         self.Lec_name = Lec_name
+         
+
+
+class Tag():
+	def __init__(self,tag_name):
+		self.tag_name = tag_name
+        
+        
+class Course(Tag, Lecture):
+    def __init__(self,corse_name, tag_name,Lec_name):
+        super().__init__(tag_name) # INH form Tag class
+        super().__init__(Lec_name) # INH form Lecture class
+        self.corse_name = corse_name
+
